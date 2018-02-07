2a9703dce1e2da2148ae1deefc9a3b3046a92341
Vladislav <m.vladislav.krapivin@gmail.com>
2018-02-07 18:49:04 +0200

    Update build.py

--- build.py
+++ build.py
@@ -5,13 +5,13 @@ if __name__ == "__main__":
     builder = ConanMultiPackager(args="--build missing")
     if platform.system() == "Windows":
         builder.add(settings={"arch": "x86", "build_type": "Debug", "compiler": "Visual Studio", "compiler.version": 14, "compiler.runtime": "MTd"},
-            options={"pg_root": "C:/Program Files/PostgreSQL/10"}, env_vars={}, build_requires={})
+            options={"sqlpp11-connector-postgresql:pg_root": "C:/Program Files/PostgreSQL/10"}, env_vars={}, build_requires={})
         builder.add(settings={"arch": "x86", "build_type": "Release", "compiler": "Visual Studio", "compiler.version": 14, "compiler.runtime": "MT"},
-            options={"pg_root": "C:/Program Files/PostgreSQL/10"}, env_vars={}, build_requires={})
+            options={"sqlpp11-connector-postgresql:pg_root": "C:/Program Files/PostgreSQL/10"}, env_vars={}, build_requires={})
         builder.add(settings={"arch": "x86_64", "build_type": "Debug", "compiler": "Visual Studio", "compiler.version": 14, "compiler.runtime": "MTd"},
-            options={"pg_root": "C:/Program Files/PostgreSQL/10"}, env_vars={}, build_requires={})
+            options={"sqlpp11-connector-postgresql:pg_root": "C:/Program Files/PostgreSQL/10"}, env_vars={}, build_requires={})
         builder.add(settings={"arch": "x86_64", "build_type": "Release", "compiler": "Visual Studio", "compiler.version": 14, "compiler.runtime": "MT"},
-            options={"pg_root": "C:/Program Files/PostgreSQL/10"}, env_vars={}, build_requires={})
+            options={"sqlpp11-connector-postgresql:pg_root": "C:/Program Files/PostgreSQL/10"}, env_vars={}, build_requires={})
     else:
         builder.add(settings={"arch": "x86", "build_type": "Debug", "compiler.libcxx": "libstdc++11"},
             options={}, env_vars={}, build_requires={})
