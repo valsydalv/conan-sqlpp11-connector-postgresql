389af6ec0db420e77ff5bc328c8e33bbbd09e757
Vladislav <m.vladislav.krapivin@gmail.com>
2018-02-23 17:24:10 +0200

    Update build.py

--- build.py
+++ build.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     builder = ConanMultiPackager(args="--build missing")
     if platform.system() == "Windows":
         builder.add(settings={"arch": "x86_64", "build_type": "Debug", "compiler": "Visual Studio", "compiler.version": 14, "compiler.runtime": "MTd"},
-            options={"sqlpp11-connector-postgresql:pg_root": "C:/Program Files/PostgreSQL/10"}, env_vars={}, build_requires={})
+            options={}, env_vars={}, build_requires={})
         builder.add(settings={"arch": "x86_64", "build_type": "Release", "compiler": "Visual Studio", "compiler.version": 14, "compiler.runtime": "MT"},
-            options={"sqlpp11-connector-postgresql:pg_root": "C:/Program Files/PostgreSQL/10"}, env_vars={}, build_requires={})
+            options={}, env_vars={}, build_requires={})
     else:
         builder.add(settings={"arch": "x86", "build_type": "Debug", "compiler.libcxx": "libstdc++11"},
             options={}, env_vars={}, build_requires={})
